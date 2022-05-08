8d08bba085394c58cc37e2231e9c99de1fa2fe75
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-08 14:02:31 -0600

    cambiando algunas cosas

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -9,64 +9,108 @@
 // Para correr el ambiente, se escribe: npm run dev
 // Para que las instancias funcionen, se tienen que hacer acá.
 
+// Referencias: 
+// 1. https://www.youtube.com/watch?v=hpfDRnijdPE&ab_channel=DigitalSolutionsMaster
+// 2. https://www.youtube.com/watch?v=CMwt6Nwjf9g&ab_channel=BetoMoedano
+// 3. https://www.youtube.com/watch?v=56fX1h_b1H0&ab_channel=BetoMoedano
+
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import suma from './operaciones/suma'
+import resta from './operaciones/resta'
+import multiplicacion from './operaciones/multiplicacion'
 
 function App() {
 
-  const [resultado, setResultado] = useState("") // Estado de los botones de la calculadora.
+  const [op1, setOp1] = useState("") // Estado de los botones de la calculadora. Guarda el primer valor.
+  const [op2, setOp2] = useState("") // Estado de los botones de la calculadora. Guarda el segundo valor.
+  const [operacion, setOperacion] = useState("") // Estado que guarda las operaciones. 
+  const [resultado, setResultdado] = useState(0) // Estado que guarda los resultados.
 
-  // Eventlistener de los botones.
+  // Eventlistener de los botones. Este escucha nada más a los botones de los números.
   const handleClick = (e) => {
-    setResultado(resultado.concat(e.target.name)) 
+
+    // Si la operación está vacía, entonces se asigna el primer número al primer estado.
+    if(operacion === ""){
+      setOp1(op1 + e) // Settea el primer número de la calculadora.
+    }else{
+      // Si ya se seleccionó la operación, entonces se asigna el valor del segundo número al segundo estado.
+      setOp2(op2 + e) // Settea el segundo número de la calculadora.
+    }
+  }
+
+  // Eventlistener de los botones. Este escucha a los botones de las operaciones.
+  const opera = (e) => {
+    setOperacion(e)
   }
 
   // Método para borrar la pantalla.
   const clear = () => {
-    setResultado("")
+    setOp1("")
+    setOp2("")
+    setOperacion("")
+    setResultdado("")
+  }
+
+  // Eventlistener de los resultados. Este escucha al igual de la calculadora.
+  const resultads = () => {
+    switch (operacion){
+      case "+":
+        setResultdado(suma(Number(op1), Number(op2)))
+        break;
+      case "-":
+        const b = resta(Number(op1), Number(op2))
+        setResultdado(b)
+        break;
+      case "-":
+        const c = multiplicacion(Number(op1), Number(op2))
+        setResultdado(c)
+        break;
+    }
   }
 
   return (
     <div className="App">
-    
-      <div className="container">
-        {/*Div para el display de la calculadora*/}
-        <form>
-          <input type="text" value={resultado} />
-        </form>
 
+      {/*Div para el display de la calculadora*/}
+      {/* */}
+      <div className="container">
+        {/*Imprime el primer número con el operando*/}
+        <div className="prev">{operacion ? op1 : ""}</div>
+        {/*Imprime el resultado o la operación*/}
+        <div className="current">{resultado ? (!operacion ? op1 : op2) : resultado}</div>
       </div>
       {/*Div para la calculadora*/}
       <div className="Calculadora">
         {/*Botón para el cero*/}
-        <button name="0" className="Cero" onClick={handleClick}>0</button>
+        <button className="Cero" onClick={() => {handleClick(0)}}>0</button>
         {/*Botón para el uno*/}
-        <button name="1" className="Uno" onClick={handleClick}>1</button>
+        <button className="Uno" onClick={() => {handleClick(1)}}>1</button>
         {/*Botón para el dos*/}
-        <button name="2" className="Dos" onClick={handleClick}>2</button>
+        <button className="Dos" onClick={() => {handleClick(2)}}>2</button>
         {/*Botón para el tres*/}
-        <button name="3" className="Tres" onClick={handleClick}>3</button>
+        <button className="Tres" onClick={() => {handleClick(3)}}>3</button>
         {/*Botón para el cuatro*/}
-        <button name="4" className="Cuatro" onClick={handleClick}>4</button>
+        <button className="Cuatro" onClick={() => {handleClick(4)}}>4</button>
         {/*Botón para el seis*/}
-        <button name="5" className="Cinco" onClick={handleClick}>5</button>
+        <button className="Cinco" onClick={() => {handleClick(5)}}>5</button>
         {/*Botón para el siete*/}
-        <button name="6" className="Seis" onClick={handleClick}>6</button>
+        <button className="Seis" onClick={() => {handleClick(6)}}>6</button>
         {/*Botón para el siete*/}
-        <button name="7" className="Siete" onClick={handleClick}>7</button>
+        <button className="Siete" onClick={() => {handleClick(7)}}>7</button>
         {/*Botón para el ocho*/}
-        <button name="8" className="Ocho" onClick={handleClick}>8</button>
+        <button className="Ocho" onClick={() => {handleClick(8)}}>8</button>
         {/*Botón para el nueve*/}
-        <button name="9" className="Nueve" onClick={handleClick}>9</button>
+        <button className="Nueve" onClick={() => {handleClick(9)}}>9</button>
         {/*Botón para el más*/}
-        <button name="+" className="Mas" onClick={handleClick}>+</button>
+        <button className="Mas" onClick={() => {opera("+")}}>+</button>
         {/*Botón para el menos*/}
-        <button name="-" className="Menos" onClick={handleClick}>-</button>
+        <button className="Menos" onClick={() => {opera("-")}}>-</button>
         {/*Botón para la multiplicación*/}
-        <button name="x" className="Multiplicacion" onClick={handleClick}>x</button>
+        <button className="Multiplicacion" onClick={() => {opera("*")}}>x</button>
         {/*Botón para la igualdad*/}
-        <button className="Igual" onClick={handleClick}>=</button>
+        <button className="Igual" onClick={resultads}>=</button>
         {/*Botón para borrar*/}
         <button className="Borrar" onClick={clear} id="borrar" >C</button>
       </div>
